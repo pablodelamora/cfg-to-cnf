74cdda770373a3e649627a12fb4144829f742020
rafael.zambianco <rafael.zambianco@gmail.com>
2011-07-04 17:02:14 +0000

    submit hello

--- LgensFormaisAutomatos/src/Hello.java
+++ LgensFormaisAutomatos/src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
     public static void main(String args[]) {
 
-        System.out.println("Hello World!!!");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello World!!!");
+        }
     }
 }
